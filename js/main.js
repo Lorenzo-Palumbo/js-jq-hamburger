cf98dcbac50f3f9f5495044eb6ba4760340cad94
Unknown <lorenzo.palumbo@hotmail.com>
2020-02-27 17:18:15 +0100

    completo

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.header-right > a').click(function(){
+    $('.hamburger-menu').addClass('active');
+});
+$('.hamburger-menu > a').click(function(){
+    $('.hamburger-menu').removeClass('active');
+});
